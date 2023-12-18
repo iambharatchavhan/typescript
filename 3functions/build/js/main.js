8c8e50cbe41698c73e15106250b51ea92c60457c
bharat chavhan <bharatchavhan141@gmail.com>
2023-12-18 21:50:40 +0530

    fuctions in typescript

--- 3functions/build/js/main.js
+++ 3functions/build/js/main.js
@@ -14,7 +14,7 @@ zeroUsername = "patlu";
 console.log(zeroUsername);
 let stateStatus;
 let errorCodes;
-// it mean literaly values other than assign not allowed
+// it mean literally values other than assign not allowed
 // what is return void
 const message = (msg) => {
     console.log(msg);
@@ -26,16 +26,37 @@ message(add(20, 20));
 let multiply = (a, b) => a * b;
 message(multiply(2, 5));
 // if we know what type of parameters function accept and what will return then we will make the blueprint for the type alias its a benifit of it
-//* optional paramiters in functions 
-// alias will not work on optianl parameters or defualts params
+//* optional parameters in functions 
+// alias will not work on optional parameters or defualts params
 const sumOfThreeOrTwo = (a, b, c) => {
     // now first have to check if third param is passed or not cos if not passed it will be undefined 
     return c !== undefined ? a + b + c : a + b;
 };
 message(sumOfThreeOrTwo(5, 5, 5));
 message(sumOfThreeOrTwo(5, 20));
-// note: optianal chain param will has always on the way last postiton in params
+// note: optional chain param will has always on the way last position in params
 const sum = function (a, b) {
     return a + b;
 };
 message(sum(23, 45));
+//rest parameter in functions
+// lets see one good example 
+const addAll = (...nums) => {
+    return nums.reduce((prev, curr) => { return prev + curr; });
+};
+message(addAll(1, 2, 3, 4));
+// we have to send rest parameter at last like optional chain
+// like 
+const sumOfAll = (a, ...nums) => {
+    return a + nums.reduce((prev, curr) => {
+        return prev + curr;
+    });
+};
+message(sumOfAll(1, 2, 3, 4));
+// default parameters 
+const defaultParam = (a = 10, b, c = 2) => {
+    let sum = a + b + c;
+    return sum;
+};
+message(defaultParam(10, 3)); // == >15  a=10 ,b=3, c=2 this c is default type parameter
+message(defaultParam(undefined, 10)); // 
--- 3functions/src/main.ts
+++ 3functions/src/main.ts
@@ -33,11 +33,11 @@ console.log(zeroUsername)
 zeroUsername = "patlu"
 console.log(zeroUsername)
 
-let stateStatus : "pending" | "resovle" | "rejected"
+let stateStatus : "pending" | "resolve" | "rejected"
 
 let errorCodes : 404 | 200 
 
-// it mean literaly values other than assign not allowed
+// it mean literally values other than assign not allowed
 
 // what is return void
 
@@ -45,7 +45,7 @@ const message = (msg:any): void =>{
     console.log(msg)
 }
 
-// the function which does not return anuthing returns type void 
+// the function which does not return anything returns type void 
 
 // type alias for functions
 
@@ -63,9 +63,9 @@ message(multiply(2,5))
 // if we know what type of parameters function accept and what will return then we will make the blueprint for the type alias its a benifit of it
 
 
-//* optional paramiters in functions 
+//* optional parameters in functions 
 
-// alias will not work on optianl parameters or defualts params
+// alias will not work on optional parameters or defualts params
 
 const sumOfThreeOrTwo = (a:number ,b:number ,c?:number) => {
     // now first have to check if third param is passed or not cos if not passed it will be undefined 
@@ -76,22 +76,41 @@ return c !== undefined ?  a + b + c : a + b
 message(sumOfThreeOrTwo(5,5,5))
 message(sumOfThreeOrTwo(5,20))
 
-// note: optianal chain param will has always on the way last postiton in params
-
-
-
-
-
-
-
-
-
-
+// note: optional chain param will has always on the way last position in params
 
 const sum  = function (a:number,b:number):number {
     return a + b 
 }
 message(sum(23,45)) 
+
+//rest parameter in functions
+
+// lets see one good example 
+
+const addAll = (...nums:number[]):number => {
+     return   nums.reduce((prev,curr)=>{ return prev + curr })
+}
+message(addAll(1,2,3,4))      
+
+// we have to send rest parameter at last like optional chain
+// like 
+
+ const sumOfAll = (a:number,...nums:number[]):number=>{
+       return a + nums.reduce((prev,curr)=>{
+        return prev + curr
+       })
+ }
+ message(sumOfAll(1,2,3,4))
+
+
+ // default parameters 
+
+ const defaultParam = (a:number = 10  ,b:number , c:number =2  ):number =>{
+            let sum:number = a + b + c
+            return sum
+ }  
+message(defaultParam(10,3)) // == >15  a=10 ,b=3, c=2 this c is default type parameter
+message(defaultParam(undefined,10)) // 
 
 
 
